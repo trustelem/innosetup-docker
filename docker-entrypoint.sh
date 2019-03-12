fdfd3c6ffd3cbf5f53fcfa4d079f77705c6e2176
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-03-12 09:50:25 +0900

    Remove unneeded files

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# we need to tell GUI applications which display to use
-export DISPLAY=xserver:0
-
-# we also need to use the proper magic cookie for authorization
-xauth merge /Xauthority/xserver.xauth -q
